d8a737f6b0d6ed94fc01e787e65967bd16f9e33f
Felipe Badia <137180694+felipebdia@users.noreply.github.com>
2023-10-03 13:43:32 -0300

    background

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function carregar() {
     if (hora >= 0  && hora < 12) {
         //BOM DIA
         img.src = 'img/manha.png'
+        document.body.style.background = '#AC8C25'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE
         img.src = 'img/tarde.png'
+        document.body.style.background = '#D5D3D2'
     } else {
         // BOA NOITE
         img.src = 'img/noite.png'
+        document.body.style.background = '#322944'
     }
 }
